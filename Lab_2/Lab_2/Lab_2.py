16784bd75315c409859d59cafb60bbef6dc3ab9b
Isaac <kingnerdthethird@outlook.com>
2018-03-14 10:17:00 -0400

    shit don't work

--- Lab_2/Lab_2/Lab_2.py
+++ Lab_2/Lab_2/Lab_2.py
@@ -5,7 +5,7 @@ from numpy.random import random as rng
 import numpy as np
 import matplotlib.pyplot as plt
 
-num_steps=1000
+"""num_steps=1000
 
 x_i,y_i=0,0
 x=[]
@@ -36,5 +36,42 @@ for i in range(0,1000):
     
 plt.scatter(pos_final[0],pos_final[1])
 plt.hist(pos,num_steps)
-plt.show()
+plt.show()"""
 
+x = np.zeros(1000)
+y = np.zeros(1000)
+
+
+
+for i in range(0, 1):
+    x_samples = rng(1000)
+    y_samples = rng(1000)
+
+    for i in range(1, 1000):
+        if x_samples[i] < 0.5:
+            x[i] = x[i - 1] + 1
+        else:
+            x[i] = x[i - 1] - 1
+
+        if y_samples[i] < 0.5:
+            y[i] = y[i - 1] + 1
+        else:
+            y[i] = y[i - 1] - 1
+
+    """plt.figure("No Line Plot")
+    plt.plot(x, y)
+    plt.figure("Line Plot")
+    plt.plot(x, y, '--')
+    plt.figure("Scatter")
+    plt.scatter(x, y, s=2)"""
+    f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, sharex='col', sharey='row')
+    ax1.plot(x, y)
+    ax1.set_title('Normal Plot')
+    ax2.scatter(x, y, s=2)
+    ax2.set_title('Normal Scatter')
+    ax3.scatter(x, y, color='r')
+    ax3.set_title('I Don\'t Know How To Omit This')
+    ax4.plot(x, y, '--')
+    ax4.set_title('Plot But With Dashed Lines')
+
+plt.show()
